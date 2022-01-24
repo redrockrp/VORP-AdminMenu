625e2deb091e7e671fc69809c1b9f97fba180df2
Dmitry [KP0H™] Pelevin <dmitry@pelevin.pro>
2022-01-24 13:58:00 +0300

    Alt+G shortcut for Noclip2

---
+++ VORP_AdminMenu[Client-Server]/vorpadminmenu_cl/Actions/NoClipAction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using vorpadminmenu_cl.Functions.Boosters;
+using vorpadminmenu_cl.Menus;
+
+namespace vorpadminmenu_cl.Actions
+{
+	public class NoClipAction : BaseScript
+	{
+		public NoClipAction()
+		{
+            Tick += NoClipAction_Tick;
+		}
+
+        [Tick]
+        private async Task NoClipAction_Tick()
+        {
+            if (GetUserInfo.userGroup == "user")
+            {
+                return;
+            }
+
+            List<object> list = new List<object>();
+            //ALT + G
+            if (API.IsControlPressed(0, 0x8AAA0AD4) && API.IsControlJustPressed(0, 0x760A9C6F))
+            {
+                bool nc = Boosters.Getmclip();
+                Boosters.Setmclip(!nc);
+                BoosterFunctions.Noclip2(list);
+            }
+        }
+    }
+}
+
